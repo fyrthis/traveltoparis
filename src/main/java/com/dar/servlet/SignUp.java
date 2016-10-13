06f04ee7b3e24f59411dc55bdd3dca6348ddf50c
tanguinoche <tanguy.retail@caramail.com>
2016-10-13 17:46:29 +0200

    essai

--- src/main/java/com/dar/servlet/SignUp.java
+++ src/main/java/com/dar/servlet/SignUp.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.dar.servlet;
 
 import java.io.IOException;
@@ -19,25 +18,3 @@ public class SignUp extends HttpServlet {
 	
 	
 }
-=======
-package com.dar.servlet;
-
-import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public class SignUp extends HttpServlet {
-
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher view = request.getRequestDispatcher("html/signup.html");
-		view.forward(request, response);
-	}
-	
-	
-}
->>>>>>> 767a9f5df84ef8006ae63bbaba7f7e0130a3b472
