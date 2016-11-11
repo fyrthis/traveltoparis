3319394d959177e60eb72f07304ea57540297bf9
Anastasios Doumoulakis <a.doumoulakis@gmail.com>
2016-11-11 21:15:21 +0100

    fixed create trip

--- src/main/java/com/dar/servlet/service/update/CreateTrip.java
+++ src/main/java/com/dar/servlet/service/update/CreateTrip.java
@@ -6,13 +6,16 @@ import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashMap;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.dar.backend.sql.SQLManager;
 
@@ -31,7 +34,10 @@ public class CreateTrip extends HttpServlet {
         String description = request.getParameter("description");
         String begins = request.getParameter("begins");
         String ends = request.getParameter("ends");
-
+        System.out.println("name " + name);
+        System.out.println("des " + description);
+        System.out.println("begin " + begins);
+        System.out.println("end " + ends);
         Date date1;
         Date date2;
         Calendar cal = Calendar.getInstance();
@@ -48,22 +54,32 @@ public class CreateTrip extends HttpServlet {
             cal.set(Calendar.MONTH, Integer.parseInt(list[1]));
             cal.set(Calendar.DAY_OF_MONTH, Integer.parseInt(list[2]));
             date2 = new Date(cal.getTimeInMillis());
+
             SQLManager mngr = new SQLManager();
             Connection conn = mngr.getConnection();
             stmt = conn.prepareStatement(rq);
+            //stmt.setQueryTimeout(10);
             stmt.setString(1, name);
             stmt.setString(2, description);
             stmt.setDate(3, date1);
             stmt.setDate(4, date2);
-            stmt.executeUpdate();
-            //Le insert into nous retourne l'id du trip nouvellement ajouté.
-            ResultSet newly_trip = stmt.getResultSet();
-            int id_trip = newly_trip.getInt(1);
+            ArrayList<HashMap<String, Object>> res = mngr.executeQuery(stmt);
+            int id_trip = (Integer)res.get(0).get("id_trip");
+
+            HttpSession session = request.getSession();
+
+            rq = "INSERT INTO involded (id_user, id_trip, is_admin) SELECT u.id_user, ?, TRUE FROM users u WHERE u.login=?;";
+            stmt = conn.prepareStatement(rq);
+            stmt.setInt(1,id_trip);
+            stmt.setString(2, (String)session.getAttribute("uname"));
+            System.out.println("Result : " + mngr.executeUpdate(stmt));
             conn.close();
-            RequestDispatcher view = request.getRequestDispatcher("html/trip.html?id="+id_trip);
-    		view.forward(request, response);
-        } catch(Exception e) {e.printStackTrace();}
-        
+            //RequestDispatcher view = request.getRequestDispatcher("html/trip.html?id="+id_trip);
+            response.sendRedirect(request.getContextPath() + "/html/trip.html?id=" + id_trip);
+        } catch(Exception e) {
+            e.printStackTrace();
+            System.out.flush();
+        }
     }
 
 }
