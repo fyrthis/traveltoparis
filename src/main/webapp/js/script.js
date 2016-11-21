3f0429d104878dbe51ee28b06a3c8c544f7fbbd1
tanguinoche <tanguy.retail@caramail.com>
2016-11-21 13:39:59 +0100

    fixed add friends

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -114,16 +114,18 @@ $(document).ready(function(){
     var i = 0;
     function addFriend() {
         var add = $('#add');
-        var changing = $('#changing');
         add.attr('id','changing');
-        changing.attr('src', 'minus.png');
-        changing.unbind(); //On enlève tous les listeners, et en particulier celui de addFriend qui persiste.
-        changing.on('click', function() {
-            $(this).parent().parent().fadeOut(700, function() { $(this).remove(); })
+        
+        add.toggleClass('glyphicon-plus');
+        add.toggleClass('glyphicon-minus');
+        add.unbind(); //On enlève tous les listeners, et en particulier celui de addFriend qui persiste.
+        add.on('click', function() {
+            $(this).parent().parent().fadeOut(500, function() { $(this).remove(); })
         });
 
-        $('<tr><td><input type="email" name="friend"></td> <td><img id="add" src="plus.png"></td></tr>').fadeIn(1000).insertAfter(changing.parent().parent());
-        changing.removeAttr('id');
+        $('<tr><td><input type="email" name="friend" class="form-control"></td><td><i id="add" class="glyphicon glyphicon-plus"></i></td></tr>').fadeIn(500).insertAfter(add.parent().parent());
+        add.removeAttr('id');
+        var add = $('#add');
         add.on('click', function() { addFriend(); });
     }
     $('#add').on('click', function() {addFriend();});
